99b3d09ab3a4e5afdee1dc1b5254001783f38424
Anders Kleimark <akleimark@gmail.com>
2023-02-06 18:49:24 +0100

    Jag lade till dokumentation.

--- ChessMove.h
+++ ChessMove.h
@@ -14,10 +14,7 @@ class ChessMove
         ChessMove(const unsigned int &fSquare, const unsigned int &tSquare, const unsigned int &pFlag = QUIET_MOVE):
             fromSquare(fSquare), toSquare(tSquare), flag(pFlag), score(0) {}
 
-        /**
-         * @brief getFromSquare ger startrutan enligt 0x88-systemet.
-         * @return
-         */
+
         unsigned int getFromSquare() const { return fromSquare; }
         unsigned int getToSquare() const { return toSquare; }
         unsigned int getFlag() const { return flag; }
@@ -26,9 +23,14 @@ class ChessMove
         bool operator==(const ChessMove &chessMove);
 
     private:
+        /// Den här datamedlemmen anger från vilken ruta, som draget utgår från. Rutan anges enligt 0x88-systemet.
         unsigned int fromSquare;
+        /// Den här datamedlemmen anger destinationsrutan. Rutan anges enligt 0x88-systemet.
         unsigned int toSquare;
+        /// Den här datamedlemmen anger vilken typ av drag, som det rör sig om. Exempel på typer av drag är QUIET_MOVE, CASTLE_MOVE med flera.
         unsigned int flag;
+        /// Den här datamedlemmen anger vilken poäng som draget har. Ett drag med en högre poäng rankas högre och prövas tidigare, än ett drag med en lägre poäng.
+        /// Drag som involverar ett slag ger exempelvis generellt en högre poäng, än "vanliga och lugna drag".
         unsigned int score;
 
 };
